136897652dcf671f2e0659b6a8ed5ad9eb4b82e8
Zoltan <zpap@macadamian.com>
2013-04-14 11:22:38 +0300

    Providing real data from CO2.xml

--- webservice/server.js
+++ webservice/server.js
@@ -1,6 +1,52 @@
 var express = require("express");
 var app = express();
 
+// ----------------------------------------- XML --------------------------------
+
+var fs = require('fs'),
+xml2js = require('xml2js');
+jquery = require('jquery');
+
+var parser = new xml2js.Parser({ explicitArray: true, attrkey: "SHAttr", charkey: "SHCharKey" });
+// storing data based on xml
+var availableDates = new Array();
+var longitudes;
+var latitudeList = new Array();
+
+console.log("Parsing CO2.xml ...");
+
+// parsing the xml
+fs.readFile('CO2.xml', function(err, data) {
+    parser.parseString(data, function (err, result) {
+
+        var jsonString = JSON.stringify(result);
+        var json_parsed = jquery.parseJSON(jsonString);
+        var root = json_parsed.Eco_Bodhi;
+        longitudes = root.Longitudes[0].split(",");
+        var dates = root.Date;
+
+        for (i = 0; i < dates.length; i++) {
+            var dateData = dates[i];
+            // storing latitudes list for every date, and updating date list
+            latitudeList[i] = dateData.Latitude;
+            var date = dateData.SHAttr.value;
+            availableDates[i] = date;
+        }
+
+        console.log("Total dates: " + dates.length + " parsed sucessfully!");
+
+        // Use middleware to parse POST data and use custom HTTP methods
+        app.use(express.bodyParser());
+        app.use(express.methodOverride());
+
+        var port = process.env.PORT || 5000;
+
+        app.listen(port, function() {
+            console.log("Starting Service. Listening on " + port + " ...");
+        });
+    });
+});
+
 // ------------------------------------------- Tips  -----------------------------
 
 function Tip (title, description) {
@@ -74,6 +120,40 @@ CarbonFootprintRepository.prototype.findAll = function() {
     return results;
 }
 
+CarbonFootprintRepository.prototype.carbonValues = function(long, lat) {
+
+    // finding latitude index
+
+    var indexLong = longitudes.indexOf(long);
+
+    console.log ("Longitude index requested " + indexLong + " for " + long);
+
+    console.log("Displaying carbon values" + latitudeList.length);
+    var result = new Array();
+
+    for (i = 0; i < latitudeList.length; i++) {
+
+        var latitudes = latitudeList[i];
+
+        for (j = 0; j < latitudes.length; j++) {
+
+            var latitude = latitudes[j].SHCharKey;
+            var latitudeValue = latitudes[j].SHAttr.value;
+
+            if (latitudeValue == lat) {
+
+                var latitudeItems = latitude.split(",");
+
+                var value = latitudeItems[indexLong];
+
+                result[i] = value;
+            }
+        }
+   }
+
+    return result;
+}
+
 // ------------------------------------------------------------------------------
 
 var carbonFootprintRepository = new CarbonFootprintRepository();
@@ -90,14 +170,6 @@ app.configure(function() {
 
 app.get('/', function(request, response) {
     response.send('Welcome in Seven Heaven');
-});
-
-app.get('/carbonFootprints', function(request, response) {
-    try {
-        response.json(carbonFootprintRepository.findAll());
-    } catch (exception) {
-        response.send(404);
-    }
 });
 
 app.get('/tips', function(request, response) {
@@ -124,19 +196,32 @@ app.get('/facts', function(request, response) {
 });
 
 
-app.get('/carbonFootprints', function(request, response) {
+app.get('/availableDates', function(request, response) {
     try {
-        response.json(carbonFootprintRepository.findAll());
+        response.json(availableDates);
     } catch (exception) {
         response.send(404);
     }
 });
 
-app.get('/carbonFootprint/:x/:y', function(request, response) {
-    var x = request.params.x;
-    var y = request.params.y;
+app.get('/longitudes', function(request, response) {
     try {
-       response.json(carbonFootprintRepository.find(x,y));
+
+        var errorMessage = "no error";
+        var result = "success";
+
+        response.json({data: longitudes, message: errorMessage, result: result});
+    } catch (exception) {
+        response.send(404);
+    }
+});
+
+
+app.get('/carbonFootprint/:long/:lat', function(request, response) {
+    var long = request.params.long;
+    var lat = request.params.lat;
+    try {
+        response.json(carbonFootprintRepository.carbonValues(long,lat));
     } catch (exception) {
         response.send(404);
     }
@@ -144,13 +229,4 @@ app.get('/carbonFootprint/:x/:y', function(request, response) {
 
 // ------------------------------------------------------------------------------
 
-// Use middleware to parse POST data and use custom HTTP methods
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-
-var port = process.env.PORT || 5000;
-
-app.listen(port, function() {
-    console.log("Listening on " + port);
-});
 
